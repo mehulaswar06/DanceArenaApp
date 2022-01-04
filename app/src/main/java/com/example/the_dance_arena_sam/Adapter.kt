5bad5ac22b2b6df973c6fed9daaf8ac82fe32797
mehul <aswarmehul19@siesgst.ac.in>
2022-01-05 04:11:38 +0530

    updating the adapter

---
+++ app/src/main/java/com/example/the_dance_arena_sam/Adapter.kt
@@ -0,0 +1,29 @@
+package com.example.the_dance_arena_sam
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.view.view.*
+
+class Adapter(var dance:List<danceData>): RecyclerView.Adapter<myViewHolder>() {
+    class myViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        var title = view.title
+        var danceimg=view.dance_img
+
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): myViewHolder {
+         var itemView=LayoutInflater.from(parent.context).inflate(R.layout.view,parent,false)
+         return myViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: myViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        return dance.size
+    }
+}
---
+++ app/src/main/java/com/example/the_dance_arena_sam/danceData.kt
@@ -0,0 +1,10 @@
+package com.example.the_dance_arena_sam
+
+data class danceData(
+    val id:Int?,
+    val title:String?,
+    val galaxy:String?,
+    val distance:String?,
+    val gravity:String?,
+    val overview:String?
+)
