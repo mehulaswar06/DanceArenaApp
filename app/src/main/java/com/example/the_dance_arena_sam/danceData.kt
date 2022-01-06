8360b33609813171b04dda8ec2cf8ffe8df787a5
mehul <aswarmehul19@siesgst.ac.in>
2022-01-06 13:14:47 +0530

    a

--- app/src/main/java/com/example/the_dance_arena_sam/danceData.kt
+++ app/src/main/java/com/example/the_dance_arena_sam/danceData.kt
@@ -3,8 +3,7 @@ package com.example.the_dance_arena_sam
 data class danceData(
     val id:Int?,
     val title:String?,
-    val galaxy:String?,
-    val distance:String?,
-    val gravity:String?,
+    val origin:String?,
+    val exponents:String?,
     val overview:String?
 )
