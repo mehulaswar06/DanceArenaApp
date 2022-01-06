37f9fd1b748248da5377b95fe8fa027ab6c3c726
anamika-netizen <anamikanevase4433@gmail.com>
2022-01-06 17:24:12 +0530

    Added dance form details file

---
+++ app/src/main/java/com/example/the_dance_arena_sam/DanceFormsDetails.kt
@@ -0,0 +1,11 @@
+package com.example.the_dance_arena_sam
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DanceFormsDetails : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dance_forms_details)
+    }
+}
--- app/src/main/java/com/example/the_dance_arena_sam/MainActivity.kt
+++ app/src/main/java/com/example/the_dance_arena_sam/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.the_dance_arena_sam
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -9,6 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        window.decorView.apply { systemUiVisibility= View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR or View.SYSTEM_UI_FLAG_FULLSCREEN }
         my_recycler.adapter=Adapter(setData.SetDance())
         my_recycler.layoutManager=LinearLayoutManager(this)
     }
